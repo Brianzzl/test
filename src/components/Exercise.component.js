c70bcaea1a6ec80444c50859745d830a0377f8b5
Brian <brianzzl@hotmail.com>
2019-12-10 05:42:01 -0500

    connected all function, however edit function not work properly

---
+++ src/components/Exercise.component.js
@@ -0,0 +1,17 @@
+//pure functional component
+//not in use yet
+
+// import React, { Component } from "react";
+// import { link } from "react-router-dom";
+
+// const Exercise = props => (
+//   <tr>
+//     <td>{props.exercise.username}</td>
+//     <td>{props.exercise.description}</td>
+//     <td>{props.exercise.duration}</td>
+//     <td>{props.exercise.substring(0, 10)}</td>
+//     <td>
+//       <Link to={'/edit/'+props.exercise._id}>edit</Link>|<a href="#" onClick={()=>props.delete}
+//     </td>
+//   </tr>
+// );
